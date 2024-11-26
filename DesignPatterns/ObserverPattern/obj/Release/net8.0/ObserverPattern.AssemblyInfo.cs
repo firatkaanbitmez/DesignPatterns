65066c4e5eb65eac66917bbc53c0f936c1f09b7a
firatkaanbitmez <firatbitmez@gmail.com>
2024-11-26 21:21:38 +0300

    add

--- DesignPatterns/ObserverPattern/obj/Release/net8.0/ObserverPattern.AssemblyInfo.cs
+++ DesignPatterns/ObserverPattern/obj/Release/net8.0/ObserverPattern.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("ObserverPattern")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7ce894e069770b318bbe93c4803b4c136ec0ccd3")]
 [assembly: System.Reflection.AssemblyProductAttribute("ObserverPattern")]
